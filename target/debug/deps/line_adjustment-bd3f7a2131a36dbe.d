71d652c53e05bac77946faa6a29524a00c26999b
Don Rumata <r4inslayer@gmail.com>
2023-10-24 18:27:05 +0500

    rm target directory

--- target/debug/deps/line_adjustment-bd3f7a2131a36dbe.d
+++
@@ -1,5 +0,0 @@
-/home/glacier/Downloads/Telegram Desktop/Rust/╨в╨╡╤Б╤В╨╛╨▓╨╛╨╡ ╨╖╨░╨┤╨░╨╜╨╕╨╡ ╨┤╨╗╤П Rust/target/debug/deps/line_adjustment-bd3f7a2131a36dbe.rmeta: src/lib.rs
-
-/home/glacier/Downloads/Telegram Desktop/Rust/╨в╨╡╤Б╤В╨╛╨▓╨╛╨╡ ╨╖╨░╨┤╨░╨╜╨╕╨╡ ╨┤╨╗╤П Rust/target/debug/deps/line_adjustment-bd3f7a2131a36dbe.d: src/lib.rs
-
-src/lib.rs:
